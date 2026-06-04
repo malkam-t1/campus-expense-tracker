22649849c137e84964a7e73634ec4ae194fee524
Tarun Malkam <tarunmalkam617@hotmail.com>
2026-06-04 12:50:49 +0530

    Add Category object with predefined expense categories

---
+++ app/src/main/kotlin/com/tarunmalkam/campusexpensetracker/Category.kt
@@ -0,0 +1,13 @@
+package com.tarunmalkam.campusexpensetracker
+
+object Category {
+    val all = listOf(
+        "Food",
+        "Travel",
+        "Books",
+        "Recharge",
+        "Shopping",
+        "Rent/Hostel",
+        "Other"
+    )
+}
